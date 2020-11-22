c2b00ae5be1c41133734986faa603492b765dc9e
Nick <deckards2505@gmail.com>
2020-11-22 22:06:35 +0600

    init: init project

---
+++ gulp/js-vendors.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+
+const vendorsScripts = [
+    // Insert libs src here:
+    'node_modules/swiper/swiper-bundle.min.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+    'node_modules/jquery/dist/jquery.min.js'
+];
+
+module.exports = function vendors(cb) {
+    return vendorsScripts.length
+        ? gulp.src(vendorsScripts)
+            .pipe(concat('libs.js'))
+            .pipe(gulp.dest('app/js/'))
+        : cb();
+};
